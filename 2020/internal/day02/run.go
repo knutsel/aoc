404edee664b7bd39d80ec64f676f9aad8d0b16da
Jan van Doorn <jvd@knutsel.com>
2023-11-13 17:08:41 +0100

    day 2

--- 2020/internal/day02/run.go
+++ 2020/internal/day02/run.go
@@ -1,4 +1,33 @@
 package day02
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func Run(fName string) {
+	inpBytes, _ := os.ReadFile("input.txt")
+	inpStr := string(inpBytes)
+	inputLines := strings.Split(strings.TrimSpace(inpStr), "\n")
+
+	part1Valid, part2Valid := 0, 0
+
+	for _, line := range inputLines {
+		fmt.Printf("%s ", line)
+		var first, last int
+
+		var c rune
+		var p string
+		fmt.Sscanf(line, "%d-%d %c: %s", &first, &last, &c, &p)
+		o := strings.Count(p, string(c))
+		if o >= first && o <= last {
+			part1Valid++
+		}
+		if (p[first-1] == byte(c) || p[last-1] == byte(c)) && p[first-1] != p[last-1] {
+			fmt.Printf(" 2V\n")
+			part2Valid++
+		}
+	}
+	fmt.Printf("part1:%d part2:%d\n", part1Valid, part2Valid)
 }
