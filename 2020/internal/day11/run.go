2ed3c0258974003e179c06541c38f59f6924f3f2
Jan van Doorn <jvd@knutsel.com>
2023-11-20 16:08:02 +0100

    Day11

--- 2020/internal/day11/run.go
+++ 2020/internal/day11/run.go
@@ -6,12 +6,14 @@ import (
 	"strings"
 )
 
-type coord struct {
+type point struct {
 	y int
 	x int
 }
 
-func newState(y int, x int, g []string) rune {
+type stateFunc func(y int, x int, g []string) rune
+
+func newStateP2(y int, x int, g []string) rune {
 	seatVal := g[y][x]
 
 	if seatVal == '.' {
@@ -20,7 +22,47 @@ func newState(y int, x int, g []string) rune {
 
 	neighbors := 0
 
-	for _, p := range []coord{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}} {
+	for _, p := range []point{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}} {
+		for mult := 1; mult < len(g); mult++ {
+			nY := y + p.y*mult
+			nX := x + p.x*mult
+
+			if nY < 0 || nY >= len(g) || nX < 0 || nX >= len(g[0]) {
+				break
+			}
+
+			if g[nY][nX] == 'L' {
+				break
+			}
+
+			if g[nY][nX] == '#' {
+				neighbors++
+				break
+			}
+		}
+	}
+
+	if seatVal == 'L' && neighbors == 0 {
+		return '#'
+	} // else
+
+	if seatVal == '#' && neighbors >= 5 {
+		return 'L'
+	}
+
+	return rune(g[y][x]) // not sure why I need to cast rune()
+}
+
+func newStateP1(y int, x int, g []string) rune {
+	seatVal := g[y][x]
+
+	if seatVal == '.' {
+		return '.'
+	}
+
+	neighbors := 0
+
+	for _, p := range []point{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}} {
 		nY := y + p.y
 		nX := x + p.x
 
@@ -44,14 +86,14 @@ func newState(y int, x int, g []string) rune {
 	return rune(g[y][x]) // not sure why I need to cast rune()
 }
 
-func doRound(inGrid []string) []string {
+func doRound(inGrid []string, sFunc stateFunc) []string {
 	outGrid := []string{}
 
 	for y := range inGrid {
 		row := ""
 
 		for x := range inGrid[y] {
-			n := newState(y, x, inGrid)
+			n := sFunc(y, x, inGrid)
 			row += string(n)
 		}
 
@@ -61,22 +103,11 @@ func doRound(inGrid []string) []string {
 	return outGrid
 }
 
-func printGrid(g []string) {
-	for y := range g {
-		fmt.Printf(" %s\n", g[y])
-	}
-}
-
-func Run(fName string) {
-	inpBytes, _ := os.ReadFile(fName)
-	inpStr := string(inpBytes)
-	grid := []string{}
-	grid = append(grid, strings.Split(strings.TrimSpace(inpStr), "\n")...)
+func doRounds(grid []string, sFunc stateFunc) {
 	previous := strings.Join(grid, "")
 
 	for {
-		// printGrid(grid)
-		newGrid := doRound(grid)
+		newGrid := doRound(grid, sFunc)
 		next := strings.Join(newGrid, "")
 
 		if previous == next {
@@ -90,4 +121,18 @@ func Run(fName string) {
 			grid[i] = strings.Clone(newGrid[i])
 		}
 	}
+}
+
+func Run(fName string) {
+	inpBytes, _ := os.ReadFile(fName)
+	inpStr := string(inpBytes)
+	grid := []string{}
+	grid = append(grid, strings.Split(strings.TrimSpace(inpStr), "\n")...)
+
+	doRounds(grid, newStateP1)
+
+	gridP2 := []string{}
+	gridP2 = append(gridP2, strings.Split(strings.TrimSpace(inpStr), "\n")...)
+
+	doRounds(gridP2, newStateP2)
 }
