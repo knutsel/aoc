0cf35dffc754c0717e13f484597895b1c8f07b79
Jan van Doorn <jvd@knutsel.com>
2023-11-28 15:26:57 +0100

    day15

--- 2020/internal/day15/run.go
+++ 2020/internal/day15/run.go
@@ -1,4 +1,32 @@
 package day15
 
-func Run(fName string) {
+import "fmt"
+
+func recite(rounds int) int {
+	spoken := map[int][]int{}
+	input := []int{14, 8, 16, 0, 1, 17}
+
+	for i := 0; i < len(input); i++ {
+		spoken[input[i]] = append(spoken[input[i]], i)
+	}
+
+	lastSpoken := input[len(input)-1]
+	for i := len(input); i < rounds; i++ {
+		if spokenAtArray, ok := spoken[lastSpoken]; ok {
+			if len(spokenAtArray) == 1 {
+				lastSpoken = 0
+
+				spoken[0] = append(spoken[0], i)
+			} else {
+				lastSpoken = spokenAtArray[len(spokenAtArray)-1] - spokenAtArray[len(spokenAtArray)-2]
+				spoken[lastSpoken] = append(spoken[lastSpoken], i)
+			}
+		}
+	}
+
+	return lastSpoken
+}
+
+func Run() {
+	fmt.Printf("P1:%d, P2:%d\n", recite(220), recite(30000000))
 }
--- 2020/internal/day15/run_test.go
+++ 2020/internal/day15/run_test.go
@@ -3,7 +3,5 @@ package day15
 import "testing"
 
 func TestReal(t *testing.T) {
-	t.Skip()
-	// Run("example.txt")
-	// Run("input.txt")
+	Run()
 }
