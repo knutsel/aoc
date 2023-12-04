cc9c81cdf9c10dac76d69981920851630a415b19
Jan van Doorn <jvd@knutsel.com>
2023-12-04 10:32:18 +0100

    Day03

--- 2023/day03/run.go
+++ 2023/day03/run.go
@@ -13,32 +13,44 @@ type point struct {
 	x int
 }
 
-func sumParts(s []string) int {
+func sumParts2(s []string) {
 	reSymbol := regexp.MustCompile(`[^.1234567890]`)
-	isNeighbor := map[point]bool{}
+	symbols := map[point]rune{}
+	p1, p2 := 0, 0
+	reNum := regexp.MustCompile(`\d+`)
+	conns := map[point][]int{}
 
 	for y, l := range s {
-		for _, x := range reSymbol.FindAllStringIndex(l, -1) {
-			for _, next := range []point{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}} {
-				p := point{y + next.y, x[0] + next.x}
-				isNeighbor[p] = true
-			}
+		for _, m := range reSymbol.FindAllStringIndex(l, -1) {
+			symbols[point{y, m[0]}] = rune(s[y][m[0]])
 		}
 	}
-
-	sum := 0
-	reNum := regexp.MustCompile(`\d+`)
 
 	for y, l := range s {
 		for _, m := range reNum.FindAllStringIndex(l, -1) {
-			if isNeighbor[point{y, m[0]}] || isNeighbor[point{y, m[1] - 1}] {
-				intVal, _ := strconv.Atoi(l[m[0]:m[1]])
-				sum += intVal
+		outter:
+			for i := m[0]; i < m[1]; i++ {
+				for _, next := range []point{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}} {
+					p := point{y + next.y, i + next.x}
+					if v, ok := symbols[p]; ok {
+						intVal, _ := strconv.Atoi(l[m[0]:m[1]])
+						fmt.Printf("%d connects to %c at %+v\n", intVal, v, p)
+						p1 += intVal
+						conns[p] = append(conns[p], intVal)
+						break outter
+					}
+				}
 			}
 		}
 	}
 
-	return sum
+	for p, conn := range conns {
+		if symbols[p] == '*' && len(conn) >= 2 {
+			p2 += conn[1] * conn[0]
+		}
+	}
+
+	fmt.Printf("P1:%d P2:%d\n", p1, p2)
 }
 
 func Run(fName string) {
@@ -48,5 +60,5 @@ func Run(fName string) {
 	schematic := []string{} // let's try treating them as strings
 	schematic = append(schematic, inputLines...)
 
-	fmt.Printf("P1:%d\n", sumParts(schematic))
+	sumParts2(schematic)
 }
