36de78884c4a0b849471046e3397ced1e1301dc3
Jan van Doorn <jvd@knutsel.com>
2024-12-01 11:41:56 +0100

    day1

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,5 +1,22 @@
 from aocd import get_data
 
-data = get_data(year=2023, day=1).splitlines()
+data = get_data(year=2024, day=1).splitlines()
+ldata = []
+rdata = []
+for line in data:
+    ldata.append(int(line.split()[0]))
+    rdata.append(int(line.split()[1]))
 
-print(data)
+ldata.sort()
+rdata.sort()
+
+i = 0
+dist = 0
+sim = 0
+for _ in ldata:
+    dist += abs(rdata[i] - ldata[i])
+    count = rdata.count(ldata[i])
+    sim += count * ldata[i]
+    i += 1
+
+print(f"part1: {dist} part2:{sim}")
