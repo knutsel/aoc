2f5d223040084c6832fe19d16b7dc4f181adccdc
Jan van Doorn <jvd@knutsel.com>
2024-12-04 08:24:31 +0100

    cp

--- 2024/day03.py
+++ 2024/day03.py
@@ -4,6 +4,7 @@ from aocd import get_data
 
 instr = get_data(year=2024, day=3)
 
+
 def sum(instr):
     mults = re.findall(r'mul\(\d+,\d+\)', instr)
     sum = 0
@@ -12,10 +13,25 @@ def sum(instr):
         sum += int(nums[0]) * int(nums[1])
     return sum
 
-print(sum(instr))
-# groups = re.search(r'(.*)don\'t\(\)(.*)do\(\)(.*)', instr)
-# print(sum(groups.group(1)), sum(groups.group(2))), sum(groups.group(3))
-#
-# dodontstring = re.sub(r'don\'t\(\).*do\(\)',  '', instr)
-# print(f"{sum(groups.group(1))+sum(dodontstring)+sum(groups.group(3))}")
 
+print(sum(instr))
+
+s = re.subn(r'don\'t\(\).*?do\(\)', '_____', instr, 0, re.MULTILINE)
+print(s[0])
+print(s[1], sum(s[0]))
+
+# groups = re.findall(r'(do\(\).*?)(don\'t\(\).*?)', "do()"+instr+"don't()")
+#
+# p2=0
+# for g in groups:
+#     print(g[0])
+#     p2+=sum(g[0])
+#
+# print(p2)
+# groups = re.search(r'(.*?)don\'t\(\)(.*)do\(\)(.*?)', instr)
+# # print(sum(groups.group(1)), sum(groups.group(2))), sum(groups.group(3))
+# #
+# dodontstring = re.sub(r'don\'t\(\).*do\(\)',  '', instr)
+# print(groups.groups(1)[0])
+# print(groups.groups(1)[1])
+# print(f"{sum(groups.group(1)[0])+sum(groups.group(1)[1])+sum(groups.group(1)[2])}")
---
+++ 2024/day04.py
@@ -0,0 +1,7 @@
+import re
+
+from aocd import get_data
+
+instr = get_data(year=2024, day=4)
+
+
