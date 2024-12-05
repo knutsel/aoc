c421dd99dd7c95608259cba12e25365f3f1cc6e8
Jan van Doorn <jvd@knutsel.com>
2024-12-05 11:30:06 +0100

    cleanup

--- 2024/day05.py
+++ 2024/day05.py
@@ -6,34 +6,26 @@ ordering = {}
 rev = {}
 
 
-def mysorter(item1, item2):
-    if item1 == item2:
+def mysorter(a, b):
+    if a == b:
         return 0
-    if item1 in ordering:
-        for val in ordering[item1]:
-            if item2 == val:
+    if a in ordering:
+        for val in ordering[a]:
+            if b == val:
                 return 1
     return -1
-    # if item1 > item2:
-    #     return 1
-    # else:
-    #     return -1
 
 
 lines = get_data(year=2024, day=5)
 
-# with open("example.txt") as file:
-#     lines = file.read()
-
 for oline in lines.split("\n\n")[0].splitlines():
     v1, v2 = map(int, oline.split("|"))
-    if not v1 in ordering:
+    if v1 not in ordering:
         ordering[v1] = []
     ordering[v1].append(v2)
-    if not v2 in rev:
+    if v2 not in rev:
         rev[v2] = []
     rev[v2].append(v1)
-    # print(v1, v2, ordering[v1])
 
 updates = []
 for i, uline in enumerate(lines.split("\n\n")[1].splitlines()):
@@ -48,24 +40,18 @@ for update in updates:
             if update[i + 1] not in ordering[page]:
                 right_order = False
 
-    # print(update)
     for i, page in enumerate(update[1:]):
-        # print(i, page)
         if page in rev:
             if update[i + 1 - 1] not in rev[page]:
-                # print(f" {update[i+1 - 1]} not in {rev[page]} ")
                 right_order = False
 
     if right_order:
-        # print(update, (len(update) - 1) / 2)
         p1 += update[int(round((len(update) - 1) / 2))]
     else:
         wrong_ordered_updates.append(update)
 
 for update in wrong_ordered_updates:
     u1 = sorted(update, key=cmp_to_key(mysorter), reverse=False)
-    # u1 = sorted(update,key=mysorter, reverse:True)
-    print(f"update {update} -> {u1}")
     p2 += u1[int(round((len(update) - 1) / 2))]
 
 print(p1, p2)
