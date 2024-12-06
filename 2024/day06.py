4e137afc32d4c1511c93021c4942f441cf39ffb1
Jan van Doorn <jvd@knutsel.com>
2024-12-06 16:09:59 +0100

    cleanup - takes about 7.5s

--- 2024/day06.py
+++ 2024/day06.py
@@ -18,57 +18,21 @@ def print_grid():
         print("")
 
 
-# def would_cause_loop(cur_loc, cur_dir):
-#     grid[cur_loc[0]][cur_loc[1]] = '#'
-#     been_before = [(cur_loc, cur_dir)]
-#     # print(start_loc, cur_dir, end="  ")
-#     new_obstacle_loc = (cur_loc[0] + step[cur_dir][0], cur_loc[1] + step[cur_dir][1])
-#     cur_dir = rotate[cur_dir]  # Extra turn is as if there was a blockage next
-#     # new_loc_if_fails  = (cur_loc[0] + step[cur_dir][0], cur_loc[1] + step[cur_dir][1])
-#     turn_count = 0
-#     while True:
-#         new_loc = (cur_loc[0] + step[cur_dir][0], cur_loc[1] + step[cur_dir][1])
-#         # print(f"    {new_loc}, {cur_dir}")
-#         if (new_loc, cur_dir) in been_before:
-#             # grid[new_obstacle_loc[0]][new_obstacle_loc[1]] = "O"
-#             # print(f"True! {new_obstacle_loc}")
-#             grid[cur_loc[0]][cur_loc[1]] = '#'
-#             return True
-#         if not is_inbounds(new_loc[0], new_loc[1]):
-#             # print("False -- OOB")
-#             return False
-#         if grid[new_loc[0]][new_loc[1]] == "#":
-#             cur_dir = rotate[cur_dir]
-#             turn_count += 1
-#         else:
-#             # print(f"   {cur_dir} --> {new_loc}")
-#             cur_loc = new_loc
-#         been_before.append((new_loc, cur_dir))
-#
-#     print("False - END")
-#     return False
-
-
 def walk(cur_loc, cur_dir, find_loops):
-    # print(f"start: {cur_loc}, {cur_dir}", end=" ")
     visited = set()
     vis_with_direction = set()
     while True:
         new_loc = (cur_loc[0] + step[cur_dir][0], cur_loc[1] + step[cur_dir][1])
-        # print(f"  new_loc: {cur_loc}, {cur_dir}", end=" ")
         if not is_inbounds(new_loc[0], new_loc[1]):
-            # print("br", end=" ")
             break
         if grid[new_loc[0]][new_loc[1]] == "#":
             cur_dir = rotate[cur_dir]
-            # print(f"  new_dir: {cur_dir}", end=" ")
         else:
             cur_loc = new_loc
             visited.add(cur_loc)
         if (cur_loc, cur_dir) in vis_with_direction and find_loops:
             return visited, True
         vis_with_direction.add((cur_loc, cur_dir))
-    # print("ret")
     return visited, False
 
 
@@ -91,27 +55,15 @@ for y, line in enumerate(data):
             start_loc = y, x
             direction = char
 
-print(f"loc: {start_loc}, dir: {direction}")
-
 vis, loop = walk(start_loc, direction, False)
-print_grid()
 
 p1 = len(vis)
 p2 = 0
-print_grid()
 for i, v in enumerate(vis):
-    print(f"i:{i}   {v}")
-    # if v == (6,3):
-    #     print_grid()
     grid[v[0]][v[1]] = '#'
     vis, loop = walk(start_loc, direction, True)
     if loop:
         p2 += 1
     grid[v[0]][v[1]] = '.'
-    # grid[v[0]][v[1]] = 'X'
-
-print()
-print_grid()
-
 
 print(p1, p2)
