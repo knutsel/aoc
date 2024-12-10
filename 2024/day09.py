6726358c80d051630036c8847c9d6bef818ffff2
Jan van Doorn <jvd@knutsel.com>
2024-12-10 17:00:57 +0100

    day9p2 almost

--- 2024/day09.py
+++ 2024/day09.py
@@ -1,6 +1,13 @@
-from functools import cmp_to_key
-
 from utils import get_input
+
+
+def bchecksum(blist):
+    sum = 0
+    for i, id in enumerate(block_list):
+        if id == '.':
+            continue
+        sum += i * id
+    return sum
 
 
 def mysorter(a, b):
@@ -13,9 +20,9 @@ def mysorter(a, b):
 
 
 data = get_input(for_example=False, day=9)
-print(data)
-# files = []
-# gaps = []
+# print(data)
+files = []
+gaps = []
 # for i, c in enumerate(data[0]):
 #     if i % 2:
 #         gaps.append(int(c))
@@ -24,17 +31,23 @@ print(data)
 #
 # for g in gaps:
 
-block_list =[]
+block_list = []
+at_block = 0
 for i, c in enumerate(data[0]):
+    size = int(c)
     if i % 2:
-        for _ in range(int(c)):
+        gaps.append((i, size, at_block))  # index, size-of-gap, location
+        for _ in range(size):
             block_list.append('.')
     else:
-        for _ in range(int(c)):
-            block_list.append(i//2)
+        files.append((i // 2, size, at_block))  # id, num_blocks, location
+        for _ in range(size):
+            block_list.append(i // 2)
+    at_block += size
 
 # last = block_list.pop()
 # print(block_list)
+org_blocks = block_list.copy()
 while True:
     last = block_list.pop()
     try:
@@ -46,14 +59,49 @@ while True:
 
 # print(block_list)
 
-p1 = 0
-for i, id in enumerate(block_list):
-    if id == '.':
-        break
-    p1+= i*id
+print(bchecksum(block_list))
 
-print(p1)
- # sorted_files = sorted(files, key=cmp_to_key(mysorter), reverse=False)
+block_list = org_blocks.copy()
+# print(''.join(map(str, block_list)))
+# print(map(str, block_list))
+# done = set()
+file_idx = len(files)
+while file_idx >= 0:
+    file_idx -= 1
+    id, size, org_loc = files[file_idx]
+    # done.add(id)
+    # print(''.join(map(str, block_list)))
+    # print(block_list)
+    # print(f"file:{id}, size:{size}  ", end="")
+    if '.' not in block_list[:org_loc]:
+        print ("bkrea")
+        break
+    move_to = 0
+    for i, gap in enumerate(gaps):
+        # print(f"g:{gap}  ", end="")
+        if gap[1] >= size:
+            move_to = gap[2]
+            gaps[i] = (gap[0], gap[1] - size, gap[2] + size)
+            # print(f" moving to {move_to}")
+            for jj in range(size):
+                block_list[jj + move_to] = id
+            for jj in range(org_loc, org_loc + size):
+                block_list[jj] = '.'
+            break
+    # if left > 0:
+    #     gaps[i]=left
+    # else:
+    #     gaps.pop(i)i
+# if move_to == -1:
+#     print(f"no gap for {id} size:({size})")
+# else:
+
+# print(''.join(map(str, block_list)))
+# print(block_list)
+
+print(bchecksum(block_list))
+
+# sorted_files = sorted(files, key=cmp_to_key(mysorter), reverse=False)
 # sorted_gaps = sorted(gaps)
 # print(files, gaps)
 # print(sorted_files)
