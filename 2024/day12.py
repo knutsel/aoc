899c4cd66bd06908e08e4cd86b012873bfdeae57
Jan van Doorn <jvd@knutsel.com>
2024-12-13 17:14:54 +0100

    day12 clean

--- 2024/day12.py
+++ 2024/day12.py
@@ -1,4 +1,4 @@
-from utils import get_input, print_grid
+from utils import get_input
 
 data = get_input(for_example=False, day=12)
 
@@ -20,45 +20,29 @@ def define_region(ptype, row, col, region, grid):  # is this bfs or dfs?
 
     return
 
+
 def perimeter(region):
     perimeter = 0
     for (row, col) in region:
         for (r, c) in (row, col + 1), (row, col - 1), (row - 1, col), (row + 1, col):
-            if (r,c) not in region:
-                perimeter+=1
+            if (r, c) not in region:
+                perimeter += 1
 
     return perimeter
-    # x = set()
-    # y = set()
-    # for (r, c) in region:
-    #     x.add(c)
-    #     y.add(r)
-    # return 2* (len(y)+ len(x))
+
 
 row_num = len(data)
 col_num = len(data[0])
 total_plots = row_num * col_num
-# print(f" r:{row_num}, c:{col_num} d:\n{data} ")
-print_grid(data)
 visited = set()
-start = (0, 0)
-all_regions = {}
-plant_type = data[0][0]
 total_price = 0
-# while len(visited) < total_plots:
 for row in range(row_num):
     for col in range(col_num):
         if (row, col) in visited:
             continue
         region = set()
         define_region(data[row][col], row, col, region, data)
-        print(f"{data[row][col]}: {region} {len(region)} -> {perimeter(region)} {len(visited)}")
-        total_price += perimeter(region)*len(region)
+        total_price += perimeter(region) * len(region)
         visited = visited | region
 
-#             if plant_type == data[row][col]:
-#                 region.add((row, col))
-
-
-define_region(data[0][0], 0, 0, region, data)
 print(total_price)
