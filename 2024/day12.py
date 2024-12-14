d1310971ad898e9fb62065ab72a742f6b82f0478
Jan van Doorn <jvd@knutsel.com>
2024-12-14 09:56:10 +0100

    day12p2, with a little help from Internet

--- 2024/day12.py
+++ 2024/day12.py
@@ -1,6 +1,6 @@
-from utils import get_input
+from utils import get_input, print_grid
 
-data = get_input(for_example=False, day=12)
+grid = get_input(for_example=False, day=12)
 
 
 def is_inbounds(y, x):
@@ -18,31 +18,42 @@ def define_region(ptype, row, col, region, grid):  # is this bfs or dfs?
             region.add((r, c))
             define_region(ptype, r, c, region, grid)
 
-    return
-
 
 def perimeter(region):
-    perimeter = 0
+    perimeter = numsides = 0
     for (row, col) in region:
-        for (r, c) in (row, col + 1), (row, col - 1), (row - 1, col), (row + 1, col):
-            if (r, c) not in region:
-                perimeter += 1
+        # Perimeter
+        perimeter += (row - 1, col) not in region
+        perimeter += (row + 1, col) not in region
+        perimeter += (row, col - 1) not in region
+        perimeter += (row, col + 1) not in region
+        # Outer corners
+        numsides += (row - 1, col) not in region and (row, col - 1) not in region
+        numsides += (row + 1, col) not in region and (row, col - 1) not in region
+        numsides += (row - 1, col) not in region and (row, col + 1) not in region
+        numsides += (row + 1, col) not in region and (row, col + 1) not in region
+        # Inner corners
+        numsides += (row - 1, col) in region and (row, col - 1) in region and (row - 1, col - 1) not in region
+        numsides += (row + 1, col) in region and (row, col - 1) in region and (row + 1, col - 1) not in region
+        numsides += (row - 1, col) in region and (row, col + 1) in region and (row - 1, col + 1) not in region
+        numsides += (row + 1, col) in region and (row, col + 1) in region and (row + 1, col + 1) not in region
+    return perimeter, numsides
 
-    return perimeter
 
-
-row_num = len(data)
-col_num = len(data[0])
+row_num = len(grid)
+col_num = len(grid[0])
 total_plots = row_num * col_num
 visited = set()
 total_price = 0
+total_price_with_discount = 0
 for row in range(row_num):
     for col in range(col_num):
         if (row, col) in visited:
             continue
         region = set()
-        define_region(data[row][col], row, col, region, data)
-        total_price += perimeter(region) * len(region)
+        define_region(grid[row][col], row, col, region, grid)
+        peri, nsides = perimeter(region)
+        total_price += peri * len(region)
+        total_price_with_discount += nsides * len(region)
         visited = visited | region
-
-print(total_price)
+print(total_price, total_price_with_discount)
