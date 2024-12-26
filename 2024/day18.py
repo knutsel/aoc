31bf173a70eae0a6a732116ad63e0a71ccf6af7a
Jan van Doorn <jvd@knutsel.com>
2024-12-26 11:06:54 +0100

    based on day16, was easy

---
+++ 2024/day18.py
@@ -0,0 +1,89 @@
+import heapq
+
+from utils import get_input, print_grid
+
+
+def is_inbounds(y, x):
+    if x < 0 or y < 0 or x >= len(grid[0]) or y >= len(grid):
+        return False
+    return True
+
+
+def make_g(grid):
+    graph = {}
+    steps = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for y, _ in enumerate(grid):
+        for x, _ in enumerate(grid[y]):
+            nnodes = {}
+            for step in steps:
+                neighbor = (y + step[0], x + step[1])
+                if is_inbounds(neighbor[0], neighbor[1]):
+                    if grid[neighbor[0]][neighbor[1]] != '#':
+                        nnodes[neighbor] = 1
+            graph[(y, x)] = nnodes
+
+    return graph, (len(grid)-1, len(grid[0])-1), (0,0)
+
+
+def dijkstra(graph, node):
+    distances = {node: float('inf') for node in graph}
+    distances[node] = 0
+    came_from = {node: None for node in graph}
+    queue = [(0, node)]
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        for next_node, weight in graph[current_node].items():
+            # if next_node in blocked[:current_distance]:
+            #     distance_temp =100000
+            distance_temp = current_distance + weight
+            if distance_temp < distances[next_node]:
+                distances[next_node] = distance_temp
+                came_from[next_node] = current_node
+                heapq.heappush(queue, (distance_temp, next_node))
+    return distances, came_from
+
+
+example = False
+data = get_input(for_example=example, day=18)
+stop = 1024
+numrows = numcols = 71
+if example:
+    numrows = numcols = 7
+    stop = 12
+
+grid = [["." for x in range(numcols)] for y in range(numrows)]
+
+blocked = []
+for i in range(stop):  # use y, x in everything --> [line-no][char-on-line]
+    line = data[i]
+    r, c = int(line.split(",")[0]), int(line.split(",")[1])
+    blocked.append((r, c))
+    grid[r][c] = '#'
+
+graph, start, end = make_g(grid)
+distances, came_from = dijkstra(graph, start)
+current_node = end
+# numsteps = numturns = 0
+prevdir = dir = "^"
+while came_from[current_node] != start:
+    # numsteps += 1
+    if prevdir != dir:
+        # numturns += 1
+        prevdir = dir
+    if current_node[0] > came_from[current_node][0]:
+        dir = "v"
+    elif current_node[0] < came_from[current_node][0]:
+        dir = "^"
+    elif current_node[1] < came_from[current_node][1]:
+        dir = "<"
+    elif current_node[1] > came_from[current_node][1]:
+        dir = ">"
+    current_node = came_from[current_node]
+    grid[current_node[0]][current_node[1]] = dir
+
+# print(numsteps)
+print_grid(grid)
+
+# the distance works for the examples, but is off-by-four for the real thing :-(
+print(distances[end])
